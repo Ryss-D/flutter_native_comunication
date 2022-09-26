d5038f42d4facf8dcffc30f44e0186ee148f4eb4
Ryss-D <70545185+Ryss-D@users.noreply.github.com>
2022-09-26 11:03:28 -0500

    feat: added ios todo for native inplemntaiton

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,6 +4,7 @@ import Flutter
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
+    //here is were you urste the ios code
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
